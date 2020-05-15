88fe755750154a1e494924b6fcff9d2cc11f908f
vasyl <vasia14wot@gmail.com>
2020-05-15 21:42:46 +0300

    3.1. Загрузка файлов в github. [#243562]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main (String [] args) {
+        System.out.println("Vasyl");
+        System.out.println("4.10.79");
+
+    }
+}
